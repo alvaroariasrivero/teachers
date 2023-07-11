e864e4564095a59b8667a5bc5f28c57e820efa78
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2023-07-11 17:01:06 +0200

    v model added to form

--- src/components/TeacherForm.vue
+++ src/components/TeacherForm.vue
@@ -1,5 +1,17 @@
 <script setup>
+    import {ref} from 'vue';
 
+    let teacher = ref({
+        teacherName : '',
+        teacherSurename : '',
+        dni: '',
+        subjects : [],
+        docs : false
+    });
+
+    let teachers = ref([]);
+
+    let subject = ref('');
 </script>
 
 <template>
@@ -7,22 +19,22 @@
         <h3>Añadir profesor</h3>
         <div>
             <label>Nombre: </label>
-            <input type="text">
+            <input type="text" v-model="teacher.teacherName">
         </div>
         <div>
             <label>Apellidos: </label>
-            <input type="text">
+            <input type="text" v-model="teacher.teacherSurename">
         </div>
         <div>
             <label>DNI: </label>
-            <input type="text">
+            <input type="text" v-model="teacher.dni">
         </div>
         <div>
             <label>Materias: </label>
-            <input type="text">
+            <input type="text" v-model="subject">
             <button>Agregar</button>
         </div>
-        <input type="checkbox"> Documentación entregada
+        <input type="checkbox" v-model="teacher.docs"> Documentación entregada
         <button>Enviar</button> 
     </section>
     <section>
